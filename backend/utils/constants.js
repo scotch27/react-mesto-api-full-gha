fa413c74f260ce6c715b7166027d88a00d296c5d
scotch <scotch27@mail.ru>
2023-07-30 23:55:45 +0300

    Настроен .env

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,2 +1,6 @@
+require('dotenv').config();
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports.URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
-module.exports.JWT_SECRET = 'a02daea4c39b0ce483f8c67e0635fc691a242fe6e48b096fa833db20408f8102';
+module.exports.JWT_SECRET = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
--- backend/utils/getJWT.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const { NODE_ENV, JWT_SECRET } = process.env;
-
-console.log(NODE_ENV);
-
-module.exports.getJWT = function getJWT() {
-  return NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
-};

e148c8d8d5d6d4194b3f67306a9789b3a768adf0
scotch <scotch27@mail.ru>
2023-07-30 23:40:56 +0300

    Создан .env-файл

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,2 +1,4 @@
+const { getJWT } = require('./getJWT');
+
 module.exports.URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
-module.exports.JWT_SECRET = 'a02daea4c39b0ce483f8c67e0635fc691a242fe6e48b096fa833db20408f8102';
+module.exports.JWT_SECRET = getJWT;

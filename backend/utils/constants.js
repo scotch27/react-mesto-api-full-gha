fc702e856f5fef49bfc89a026b1c89a778c3180e
scotch <scotch27@mail.ru>
2023-07-30 23:44:32 +0300

    Проверка без .ENV

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,4 +1,2 @@
-const { getJWT } = require('./getJWT');
-
 module.exports.URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
-module.exports.JWT_SECRET = getJWT;
+module.exports.JWT_SECRET = 'a02daea4c39b0ce483f8c67e0635fc691a242fe6e48b096fa833db20408f8102';
